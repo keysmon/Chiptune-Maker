949d4a4e9b3190ee02ab7969c37f85aef4ee31ee
adampage <27blAzEr10>
2022-03-02 18:06:18 -0800

    Experiemented with in-program spleeter library and 2,4, and 5 stem serperation.

--- source_seperate.py
+++ source_seperate.py
@@ -1,16 +1,23 @@
 import os
+from spleeter.audio.adapter import AudioAdapter
+from spleeter.separator import Separator
 
+def inAppSeperator(file):
+    separator = Separator('spleeter:5stems')
+    separator.separate_to_file(file,"output")
 
+def cmdLineSeperator(file):
+    os.system("spleeter separate -p spleeter:2stems -o output audio_example.mp3")
 
 
 def main():
-    audio_file = "audio_example.mp3"
-    
-    
-    os.system("spleeter separate -p spleeter:2stems -o output audio_example.mp3" )
+    audio_file = "input_songs\\vocals_guitar.mp3"
+
+    inAppSeperator(audio_file)
+    #cmdLineSeperator(audio_file)
+
     return
-    
-    
-    
+
+
 if __name__ == "__main__":
     main()
