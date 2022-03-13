10a6c7724da655c08752d3af195d172e7cbd6e1e
Hang Ruan <1042295034@qq..com>
2022-03-13 15:22:19 -0700

    updated argument

--- source_seperate.py
+++ source_seperate.py
@@ -1,16 +1,28 @@
 import os
+import sys
+import vamp
+import librosa
 
 
-
-
-def main():
-    audio_file = "audio_example.mp3"
+def main(audio_file = "audio_example.mp3",num_of_stem = 2):
     
     
-    os.system("spleeter separate -p spleeter:2stems -o output audio_example.mp3" )
+    command = "spleeter separate -p spleeter:"+str(num_of_stem)+"stems -o output " + audio_file
+    os.system(command)
+    audio,sr = librosa.load("output/audio_example/vocals.wav")
+    data = vamp.collect(audio,sr,"mtg-melodia:melodia")
+    print(data)
     return
     
     
     
 if __name__ == "__main__":
-    main()
+    if len(sys.argv) == 1:
+        main()
+    elif len(sys.argv) == 2:
+        main(sys.argv[1])
+    elif len(sys.argv) == 3:
+        main(sys.argv[1],sys.argv[2])
+    else:
+        println("Error: Invalid Args!")
+     
